d9d017cda0e17cf9b12044b8020b6c7fc5fe3e9c
KlootwijkBart <61149223+KlootwijkBart@users.noreply.github.com>
2020-02-26 17:30:49 +0100

    added shit to validation code

--- Validation/Validation_datagathering.py
+++ Validation/Validation_datagathering.py
@@ -6,9 +6,8 @@
     and creates arrays of the tabular data from the file """ 
 
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import csv
+import pandas as pd
 
 
 B737rpt_data = open('B737_data/B737.rpt','r')
@@ -25,7 +24,7 @@ for i in range(9,6597): #get the coordinates of the data points
     nodelabel = int(split_lines[0])
     x_loc = float(split_lines[1])
     y_loc = float(split_lines[2])
-    z_loc = float(split_lines[3])-102.5 # place origin at leading edge to match reference frame of validation model with that of numerical model
+    z_loc = float(split_lines[3]) # place origin at leading edge to match reference frame of validation model with that of numerical model
     data_points.append([nodelabel, x_loc, y_loc, z_loc])
 node_loc = np.array(data_points)
 
@@ -323,9 +322,7 @@ for i in range(59928,59944):
     RF1.append(rf1); RF2.append(rf2); RF3.append(rf3)
 jamstraight_nodal_RF_assembly = np.transpose(np.array([nodelabel,RF_magnitude,RF1,RF2,RF3]))
 
-<<<<<<< HEAD
-print(bending_nodal_U)
-=======
+
 bending_stress = np.vstack((bending_region1,bending_region2))
 jambent_stress = np.vstack((jambent_region1,jambent_region2))
 jamstraight_stress = np.vstack((jamstraight_region1,jamstraight_region2))
@@ -355,9 +352,150 @@ jamstraight = {"stress":jamstraight_stress,
 #    nodes_writer = csv.writer(nodes_file, delimiter=',')
 #    for i in range(len(tab)):
 #        nodes_writer.writerow(tab[i])
-#    
+   
         
-            
+### IMPORT TABLE CONTAINING INFO OF WHICH ELEMENTS ARE CONNECTED TO WHICH NODE
 
-            
->>>>>>> 9eb696e7be368f7c724c946ae4ac834f2a65d96a
+f = open('nodes.txt','r')
+data = f.readlines()
+f.close()
+node_elements = []
+for line in data: 
+    line = line.rstrip('\n')
+    line = line.split(',')
+    line = [float(j) for j in line if j]
+    if line:
+        node_elements.append(line)
+#node_elements = np.array(node_elements)
+
+
+### ======== SET UP DATAFRAMES FOR EACH LOAD CASE =============================
+
+
+
+### ------------------------------------------------------------------------###
+###          load case: bending     
+### ------------------------------------------------------------------------###
+
+Smises_avg = []
+Ss12_avg   = []
+for node in node_elements:
+    Smisestab = []
+    Ss12tab   = []
+    for element in node[1:]:
+        index = np.where(bending["stress"][:,0]==element)
+        index = index[0][0]
+        Smises = bending["stress"][index,1]
+        Ss12   = bending["stress"][index,2]
+        Smisestab.append(Smises)
+        Ss12tab.append(Ss12)
+    average_Smises = np.average(np.array(Smisestab))
+    average_Ss12 = np.average(np.array(Ss12tab))
+    Smises_avg.append(average_Smises)
+    Ss12_avg.append(average_Ss12)
+
+dfbending = pd.DataFrame({
+        'node'      : node_loc[:,0], 
+        'x'         : node_loc[:,1], 
+        'y'         : node_loc[:,2],
+        'z'         : node_loc[:,3],
+        'Smises'    : Smises_avg,
+        'Ss12'      : Ss12_avg,
+        'U'         : bending["U"][:,1],
+        'Ux'        : bending["U"][:,2],
+        'Uy'        : bending["U"][:,3],
+        'Uz'        : bending["U"][:,4],
+        'RF'        : bending["RF"][:,1],
+        'RFx'       : bending["RF"][:,2],
+        'RFy'       : bending["RF"][:,3],
+        'RFz'       : bending["RF"][:,4],
+        'x+Ux'      : node_loc[:,1]+bending["U"][:,2],
+        'y+Uy'      : node_loc[:,2]+bending["U"][:,3],
+        'z+Uz'      : node_loc[:,3]+bending["U"][:,4]
+        })
+    
+### ------------------------------------------------------------------------###
+###          load case: jam bent     
+### ------------------------------------------------------------------------###
+    
+Smises_avg = []
+Ss12_avg   = []
+for node in node_elements:
+    Smisestab = []
+    Ss12tab   = []
+    for element in node[1:]:
+        index = np.where(jambent["stress"][:,0]==element)
+        index = index[0][0]
+        Smises = jambent["stress"][index,1]
+        Ss12   = jambent["stress"][index,2]
+        Smisestab.append(Smises)
+        Ss12tab.append(Ss12)
+    average_Smises = np.average(np.array(Smisestab))
+    average_Ss12 = np.average(np.array(Ss12tab))
+    Smises_avg.append(average_Smises)
+    Ss12_avg.append(average_Ss12)
+
+dfjambent = pd.DataFrame({
+        'node'      : node_loc[:,0], 
+        'x'         : node_loc[:,1], 
+        'y'         : node_loc[:,2],
+        'z'         : node_loc[:,3],
+        'Smises'    : Smises_avg,
+        'Ss12'      : Ss12_avg,
+        'U'         : jambent["U"][:,1],
+        'Ux'        : jambent["U"][:,2],
+        'Uy'        : jambent["U"][:,3],
+        'Uz'        : jambent["U"][:,4],
+        'RF'        : jambent["RF"][:,1],
+        'RFx'       : jambent["RF"][:,2],
+        'RFy'       : jambent["RF"][:,3],
+        'RFz'       : jambent["RF"][:,4],
+        'x+Ux'      : node_loc[:,1]+jambent["U"][:,2],
+        'y+Uy'      : node_loc[:,2]+jambent["U"][:,3],
+        'z+Uz'      : node_loc[:,3]+jambent["U"][:,4]
+        })
+
+    
+### ------------------------------------------------------------------------###
+###          load case: jam straight     
+### ------------------------------------------------------------------------###
+    
+Smises_avg = []
+Ss12_avg   = []
+for node in node_elements:
+    Smisestab = []
+    Ss12tab   = []
+    for element in node[1:]:
+        index = np.where(jamstraight["stress"][:,0]==element)
+        index = index[0][0]
+        Smises = jamstraight["stress"][index,1]
+        Ss12   = jamstraight["stress"][index,2]
+        Smisestab.append(Smises)
+        Ss12tab.append(Ss12)
+    average_Smises = np.average(np.array(Smisestab))
+    average_Ss12 = np.average(np.array(Ss12tab))
+    Smises_avg.append(average_Smises)
+    Ss12_avg.append(average_Ss12)
+
+dfjamstraight = pd.DataFrame({
+        'node'      : node_loc[:,0], 
+        'x'         : node_loc[:,1], 
+        'y'         : node_loc[:,2],
+        'z'         : node_loc[:,3],
+        'Smises'    : Smises_avg,
+        'Ss12'      : Ss12_avg,
+        'U'         : jamstraight["U"][:,1],
+        'Ux'        : jamstraight["U"][:,2],
+        'Uy'        : jamstraight["U"][:,3],
+        'Uz'        : jamstraight["U"][:,4],
+        'RF'        : jamstraight["RF"][:,1],
+        'RFx'       : jamstraight["RF"][:,2],
+        'RFy'       : jamstraight["RF"][:,3],
+        'RFz'       : jamstraight["RF"][:,4],        
+        'x+Ux'      : node_loc[:,1]+jamstraight["U"][:,2],
+        'y+Uy'      : node_loc[:,2]+jamstraight["U"][:,3],
+        'z+Uz'      : node_loc[:,3]+jamstraight["U"][:,4]
+        })
+
+### ===========================================================================
+
